27a0b7b0fd072e05ca78beb37f08738a5dad18da
Shumpei Nagata <nagata.syunpei@gmail.com>
2021-02-16 02:07:29 +0900

    [add] Generambaでテンプレートを作成

---
+++ DataStore/Mock.swift
@@ -0,0 +1,6 @@
+//
+//  Mock.swift
+//  DataStore
+//
+//  Created by Shumpei Nagata on 2021/02/16.
+//
---
+++ Domain/Mock.swift
@@ -0,0 +1,6 @@
+//
+//  Mock.swift
+//  Domain
+//
+//  Created by Shumpei Nagata on 2021/02/16.
+//
